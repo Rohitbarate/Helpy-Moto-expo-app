a2d3e30dcd8e2e9e3efebf4dc756aadcf810081f
Rohit Rajendra Barate <rohitbarate100@gmail.com>
2023-01-04 13:15:23 +0530

    home + towing service done

--- components/screens/HomeScreen.jsx
+++ components/screens/HomeScreen.jsx
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, TouchableOpacity, Image,View,Text } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import NearbyDrivers from "./NearbyDrivers";
